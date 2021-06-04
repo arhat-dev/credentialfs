bb5bbe5f3ef55d4b6eabb524d5731bc4f1288775
donorp <donorp@arhat.solutions>
2021-06-04 12:17:27 +0200

    feat: Add loginInterface option #7
    Signed-off-by: donorp <donorp@arhat.solutions>

--- pkg/conf/fs.go
+++ pkg/conf/fs.go
@@ -6,6 +6,8 @@ type FilesystemConfig struct {
 	Mountpoint string `json:"mountpoint" yaml:"mountpoint"`
 	Debug      bool   `json:"debug" yaml:"debug"`
 
+	LoginInterface string `json:"loginInterface" yaml:"loginInterface"`
+
 	Spec []struct {
 		PM     PasswordManagerConfig `json:"pm" yaml:"pm"`
 		Mounts []MountConfig         `json:"mounts" yaml:"mounts"`
